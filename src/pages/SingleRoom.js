22894f7c7f8120974d0c449b61c5e6f61c8017f7
anhmmo <aun@metropolia.fi>
2020-03-18 11:50:41 +0200

    add featuredRoom

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
+
+
+
 
 export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        };
+        
+    }
+
+    static contextType = RoomContext;
+
     render() {
-        return (
-            <div>
-                hello from single room page
-            </div>
-        )
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
+        
+        /* check if room is undefine( not found) with tenary operator*/
+        
+        return room ? 
+        // if true (room found)
+        <Hero hero = "roomsHero">
+            <Banner title={`${name} room`}>
+                <Link to="/rooms" className="btn-primary">
+                    back to rooms
+                </Link>
+            </Banner>
+        </Hero>
+        : 
+        // if false (room not found)
+        <div className="error">
+            <h3>no such room could be found</h3>
+            <Link to="/rooms" className="btn-primary">
+                back to rooms
+            </Link>
+        </div>
+        
     }
 }
