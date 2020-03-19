4cdfde3b06591683c97446f4881c5112ab74fa47
anhmmo <aun@metropolia.fi>
2020-03-19 11:12:11 +0200

    remove comment

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -3,17 +3,24 @@ import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
 import {RoomConsumer} from '../context'
 import Loading from './Loading'
+import { logDOM } from '@testing-library/react'
 
-//dang lam gio toi day
+
 export default function RoomContainer() {
     return (
-        <RoomContainer>
-     
-        </RoomContainer>
-        <div>
-            Room container
-            <RoomFilter />
-            <RoomList />
-        </div>
-    )
+        <RoomConsumer>
+        {
+            (value) => {
+                const{loading, sorted } = value
+                return (
+                    <div>
+                        Room container
+                        <RoomFilter />
+                        <RoomList />
+                    </div>
+                );
+            }
+         }
+        </RoomConsumer>
+    );
 }
