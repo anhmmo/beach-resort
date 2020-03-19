8ef648f4b75f48499243852af9157e96161b3947
anhmmo <aun@metropolia.fi>
2020-03-19 21:03:59 +0200

    use an other way to call RoomContainer.js

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,26 +1,57 @@
+
 import React from 'react'
 import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
-import {RoomConsumer} from '../context'
+import { withRoomConsumer } from '../context'
 import Loading from './Loading'
-import { logDOM } from '@testing-library/react'
 
 
-export default function RoomContainer() {
+function RoomContainer({context}) {
+    const {loading, sortedRooms, rooms} = context;
+    if(loading) {
+        return <Loading />;
+    }
     return (
-        <RoomConsumer>
-        {
-            (value) => {
-                const{loading, sorted } = value
-                return (
-                    <div>
-                        Room container
-                        <RoomFilter />
-                        <RoomList />
-                    </div>
-                );
-            }
-         }
-        </RoomConsumer>
+        <>
+            <RoomFilter rooms = {rooms} />
+            <RoomList rooms = {sortedRooms} />
+        </>
     );
 }
+
+export default withRoomConsumer(RoomContainer);
+
+
+
+
+
+// import React from 'react'
+// import RoomFilter from './RoomFilter'
+// import RoomList from './RoomList'
+// import {RoomConsumer} from '../context'
+// import Loading from './Loading'
+
+
+
+// export default function RoomContainer() {
+//     return (
+//         <RoomConsumer>
+//         {
+//             (value) => {
+//                 const{loading, sortedRooms, rooms } = value;
+//                 if(loading) {
+//                     return <Loading />;
+//                 }
+
+//                 return (
+//                     <div>
+//                         Room container
+//                         <RoomFilter rooms = {rooms} />
+//                         <RoomList rooms = {sortedRooms} />
+//                     </div>
+//                 );
+//             }
+//          }
+//         </RoomConsumer>
+//     );
+// }
