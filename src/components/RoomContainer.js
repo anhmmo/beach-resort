dde1c40724ffd9cafa1fcf0346768992f38d33a4
anhmmo <aun@metropolia.fi>
2020-03-19 10:44:53 +0200

    add RoomContainer.js

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import RoomFilter from './RoomFilter'
+import RoomList from './RoomList'
+import {RoomConsumer} from '../context'
+import Loading from './Loading'
+
+//dang lam gio toi day
+export default function RoomContainer() {
+    return (
+        <RoomContainer>
+     
+        </RoomContainer>
+        <div>
+            Room container
+            <RoomFilter />
+            <RoomList />
+        </div>
+    )
+}
