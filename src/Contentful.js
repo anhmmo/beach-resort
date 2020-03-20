e2b5ce5e1a4723fd7e5a2a249d593266a48475f9
anhmmo <aun@metropolia.fi>
2020-03-20 14:42:38 +0200

    delete env.development file

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space: process.env.REACT_APP_API_SPACE,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: "4dhzd9gyx0x4",
+    accessToken: "N_MnpruL2752BtwKyxBAV3sJ5rxe-5GJfveRXwJ54D8"
 });
