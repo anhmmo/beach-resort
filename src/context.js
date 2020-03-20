75d73413325244ae944f7a5e8fc39c0973eee705
anhmmo <aun@metropolia.fi>
2020-03-20 12:35:31 +0200

    change getEntry in function

--- src/context.js
+++ src/context.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import items from './data';
+//import items from './data';
+import Client from './Contentful';
+
 
 const RoomContext = React.createContext();
 // <RoomContext.Provider value = {'hello'}
@@ -22,22 +24,38 @@ class RoomProvider extends Component {
     };
     // getData
 
-    componentDidMount() {
-        //this.getData
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
 
-        this.setState({
-            rooms, 
-            featuredRooms, 
-            sortedRooms: rooms, 
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        });
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "beachResort",
+                //order: "sys.createAt"
+                order: "fields.price"
+            });
+            let rooms = this.formatData(response.items); 
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+
+            this.setState({
+                rooms, 
+                featuredRooms, 
+                sortedRooms: rooms, 
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        }
+
+        catch (error){
+
+        }
+    }
+
+    componentDidMount() {
+        this.getData();
+        
     }
 
     formatData(items) {
@@ -60,8 +78,9 @@ class RoomProvider extends Component {
 
     handleChange = event => {
         const target = event.target;
-        const value = event.type === 'checkbox' ? target.checked : target.value;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = event.target.name;
+
         this.setState(
             {
                 [name]: value
@@ -91,6 +110,19 @@ class RoomProvider extends Component {
 
         // filter by price
         tempRooms = tempRooms.filter(room => room.price <= price);
+
+        // filter by size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
+
+        // filter by breakfast
+        if(breakfast) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true);
+        }
+
+        // filter by pets
+        if(pets) {
+            tempRooms = tempRooms.filter(room => room.pets === true);
+        }
 
         //change state
         this.setState({
